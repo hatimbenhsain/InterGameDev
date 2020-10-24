1287b447d9ca0b9f7ef1a4ca19f2d3212cf4ab8a
Hatim <hb1487@nyu.edu>
2020-10-25 02:24:09 +0400

    more touches
    bubbles, end, adjustments

--- intGameDev21Sep/Assets/scripts/priestessScript.cs
+++ intGameDev21Sep/Assets/scripts/priestessScript.cs
@@ -9,6 +9,7 @@ public class priestessScript : MonoBehaviour
 	public textScript ts;
 	public string[] finalMessages;
     public bool disappearing=false;
+    public GameObject endObj;
 
     Color sr;
     // Start is called before the first frame update
@@ -25,11 +26,13 @@ public class priestessScript : MonoBehaviour
         	ts.basicMessages=finalMessages;
         }else if(ts.inZone && !ts.canvas.enabled && ts.messages==finalMessages && !disappearing){
         	disappearing=true;
+        	ts.resolved=true;
         }
         if(disappearing && sr.a>=0f){
             sr.a-=0.005f;
             transform.parent.gameObject.GetComponent<SpriteRenderer>().color=sr;
         }else if(disappearing){
+        	endObj.SetActive(true);
             this.gameObject.transform.parent.gameObject.SetActive(false);
         }
     }
